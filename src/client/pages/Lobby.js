1c36cc46ed0c6294f778995c43c91d70dbce7916
okayOki <okayoki2484@gmail.com>
2020-10-25 13:48:15 +0900

    Lobby 컴포넌트 내에 Header, Join 삽입

---
+++ src/client/pages/Lobby.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Join from '../components/Join';
+
+function Lobby(){
+    return(
+        <>
+            <Header />
+            <Join />
+        </>
+    )
+}
+
+export default Lobby;

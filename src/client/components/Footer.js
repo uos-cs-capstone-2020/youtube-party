e432fd409549163f5995952a409f3d2ce8512aa0
okayOki <okayoki2484@gmail.com>
2020-10-25 23:59:32 +0900

    Footer 로고 삽입

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -5,10 +5,13 @@ function Footer(){
     return(
         <div className="Footer">
             <a href="https://github.com/uos-cs-capstone-2020/youtube-party"
-                target="_blank">
+                target="_blank"
+                rel="noopener noreferrer">
                 https://github.com/uos-cs-capstone-2020/youtube-party
             </a>
-            <a>Contact: Discord</a>
+            <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
+                Contact: Discord
+            </a>
         </div>
     )
 }

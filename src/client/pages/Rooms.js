d598fcbbdb87ea6fa5e74097a29b57d4467a6a89
okayOki <okayoki2484@gmail.com>
2020-10-26 01:37:07 +0900

    Rooms.js 생성
    기존의 Room컴포넌트를 Rooms 컴포넌트에서 로드.
    Rooms
    ㄴHeader
    ㄴRoom
    ㄴFooter

---
+++ src/client/pages/Rooms.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Room from '../components/Room';
+import Footer from '../components/Footer';
+import './background.css';
+
+function Rooms({location}){
+    return(
+        <div className="Rooms">
+            <Header />
+            <Room location={location}/>
+            <Footer />
+        </div>
+    )
+}
+
+export default Rooms;

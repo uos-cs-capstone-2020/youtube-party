c17f957681d017495551fde08f42bbfd02fcaa94
okayOki <okayoki2484@gmail.com>
2020-10-25 13:48:30 +0900

    App->Lobby로 라우팅

--- src/client/App.js
+++ src/client/App.js
@@ -2,12 +2,13 @@ import React from "react";
 //import Data from "./fetch_user";
 
 import {BrowserRouter as Router , Route} from 'react-router-dom';
-import Room from "./components/Room"
-import Join from './components/Join';
+import Room from "./components/Room";
+import Lobby from './pages/Lobby';
+
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Join}/>
+      <Route path="/" exact component={Lobby}/>
       <Route path="/room" component={Room} />
     </Router>
   );

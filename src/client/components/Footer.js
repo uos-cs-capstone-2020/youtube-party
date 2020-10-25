39160c9b044ba150d09839e827b140fc3b04462c
okayOki <okayoki2484@gmail.com>
2020-10-25 18:23:23 +0900

    Footer 컴포넌트 생성

---
+++ src/client/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <div className="Footer">
+            <div className="copyright">https://github.com/uos-cs-capstone-2020/youtube-party</div>
+        </div>
+    )
+}
+
+export default Footer;

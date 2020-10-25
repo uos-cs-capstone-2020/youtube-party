55165a5c18f0a0ebefa2c0773da9ecfd1e9622ea
okayOki <okayoki2484@gmail.com>
2020-10-25 21:46:33 +0900

    Footer 내용, 정렬 수정

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer(){
     return(
         <div className="Footer">
-            <div className="copyright">https://github.com/uos-cs-capstone-2020/youtube-party</div>
+            <a href="https://github.com/uos-cs-capstone-2020/youtube-party"
+                target="_blank">
+                https://github.com/uos-cs-capstone-2020/youtube-party
+            </a>
+            <a>Contact: Discord</a>
         </div>
     )
 }
